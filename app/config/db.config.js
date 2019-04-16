cbc15bc37d37f2a15f4387095e9e27c5ceb3d9b4
Wendelin Gerein <info@webdelin.de>
2019-04-17 00:13:42 +0200

    New version sequelize °°°-°

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,11 +1,10 @@
 const env = require('./env.js');
- 
+
 const Sequelize = require('sequelize');
 const sequelize = new Sequelize(env.database, env.username, env.password, {
   host: env.host,
   dialect: env.dialect,
-  operatorsAliases: false,
- 
+
   pool: {
     max: env.max,
     min: env.pool.min,
@@ -13,15 +12,15 @@ const sequelize = new Sequelize(env.database, env.username, env.password, {
     idle: env.pool.idle
   }
 });
- 
+
 const db = {};
- 
+
 db.Sequelize = Sequelize;
 db.sequelize = sequelize;
- 
+
 db.user = require('../model/user.model.js')(sequelize, Sequelize);
 db.role = require('../model/role.model.js')(sequelize, Sequelize);
- 
+
 db.role.belongsToMany(db.user, { through: 'user_roles', foreignKey: 'roleId', otherKey: 'userId'});
 db.user.belongsToMany(db.role, { through: 'user_roles', foreignKey: 'userId', otherKey: 'roleId'});
 
--- server.js
+++ server.js
@@ -1,30 +1,31 @@
-var express = require('express');
-var app = express();
-var bodyParser = require('body-parser');
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
 app.use(bodyParser.json())
- 
-require('./app/router/router.js')(app);
 
-const db = require('./app/config/db.config.js');
+require('./app/router/router.js')(app)
+
+const db = require('./app/config/db.config.js')
 
 const Role = db.role;
-  
+
 // force: true will drop the table if it already exists
-db.sequelize.sync({force: true}).then(() => {
-  console.log('Drop and Resync with { force: true }');
-  initial();
-});
- 
+//db.sequelize.sync({force: true}).then(() => {
+//  console.log('Drop and Resync with { force: true }');
+//  initial();
+//});
+
 //require('./app/route/project.route.js')(app);
- 
+
 // Create a Server
-var server = app.listen(8080, function () {
- 
-  var host = server.address().address
-  var port = server.address().port
- 
+const server = app.listen(8080, function () {
+
+	const host = server.address().address
+	const port = server.address().port
+
   console.log("App listening at http://%s:%s", host, port)
 })
+
 
 
 function initial(){
@@ -32,12 +33,12 @@ function initial(){
 		id: 1,
 		name: "USER"
 	});
-	
+
 	Role.create({
 		id: 2,
 		name: "ADMIN"
 	});
-	
+
 	Role.create({
 		id: 3,
 		name: "PM"
